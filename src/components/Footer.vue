33faad4791233d3f67f6b4312c6cc12e66e7e58b
Hyungsun Lim <bokeshhys@gmail.com>
2023-06-15 19:58:06 -0700

    Add footer HTML and CSS

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,22 @@
 <template>
-<footer>Footer</footer>
+<footer>
+    <article>
+        <span>
+            Weather
+        </span>
+        <p>
+            Experience weather like never before with our nice weather website. Get real-time updates, accurate forecasts, and a user-friendly interface. Discover the perfect blend of simplicity and functionality. 
+        </p>
+    </article>
+    <section>
+        <small>
+            Copyright &copy; 2023 Weather
+        </small>
+        <a href="#">
+            BACK TO TOP
+        </a>
+    </section>
+</footer>
 </template>
 
 
@@ -15,5 +32,64 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Open Sans", sans-serif;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+a {
+  text-decoration: none;
+}
+
+p {
+    line-height: 1.5;
+}
+
+footer {
+    display: flex;
+    flex-direction: column;
+    height: 30vh;
+    background-color: black;
+    color: whitesmoke;
+    row-gap: 3vh;
+    justify-content: center;
+}
+
+footer > article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+}
+
+footer > section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 1vh;
+}
+
+footer a,
+footer span {
+    font-weight: 600;
+}
+
+footer p {
+    width: 40%;
+}
+
+footer a {
+    color: whitesmoke;
+}
+
+
 
 </style>
