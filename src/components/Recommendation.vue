7991c7febd1d32c7b1738bd1d0fb3f4dd143c7a1
Hyungsun Lim <bokeshhys@gmail.com>
2023-07-06 11:19:41 -0700

    Add the folders

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -6,35 +6,35 @@
       <article>
         <h3>Accessories</h3>
         <figure>
-          <div><img src="../assets/img/cap.png" alt="cap" /></div>
-          <div><img src="../assets/img/sunglasses.png" alt="sunglasses" /></div>
-          <div><img src="../assets/img/sunhat.png" alt="sunhat" /></div>
+          <div><img src="../assets/img/Accessories/cap.png" alt="cap" /></div>
+          <div><img src="../assets/img/Accessories/sunglasses.png" alt="sunglasses" /></div>
+          <div><img src="../assets/img/Accessories/sunhat.png" alt="sunhat" /></div>
         </figure>
       </article>
       <article>
         <h3>Top</h3>
         <figure>
-          <div><img src="../assets/img/tshirt.png" alt="tshirt" /></div>
-          <div><img src="../assets/img/polo-shirt.png" alt="polo" /></div>
+          <div><img src="../assets/img/Top/tshirt.png" alt="tshirt" /></div>
+          <div><img src="../assets/img/Top/polo-shirt.png" alt="polo" /></div>
           <div>
-            <img src="../assets/img/hawaiian-shirt.png" alt="half-shirt" />
+            <img src="../assets/img/Top/hawaiian-shirt.png" alt="half-shirt" />
           </div>
         </figure>
       </article>
       <article>
         <h3>Bottoms</h3>
         <figure>
-          <div><img src="../assets/img/shorts(1).png" alt="shorts1" /></div>
-          <div><img src="../assets/img/shorts(3).png" alt="shorts3" /></div>
-          <div><img src="../assets/img/shorts(2).png" alt="shorts2" /></div>
+          <div><img src="../assets/img/Bottoms/shorts(1).png" alt="shorts1" /></div>
+          <div><img src="../assets/img/Bottoms/shorts(3).png" alt="shorts3" /></div>
+          <div><img src="../assets/img/Bottoms/shorts(2).png" alt="shorts2" /></div>
         </figure>
       </article>
       <article>
         <h3>Shoes</h3>
         <figure>
-          <div><img src="../assets/img/sandals.png" alt="shorts3" /></div>
-          <div><img src="../assets/img/sandals (2).png" alt="shorts2" /></div>
-          <div><img src="../assets/img/flip-flops.png" alt="shorts1" /></div>
+          <div><img src="../assets/img/shoes/sandals.png" alt="shorts3" /></div>
+          <div><img src="../assets/img/shoes/sandals (2).png" alt="shorts2" /></div>
+          <div><img src="../assets/img/shoes/flip-flops.png" alt="shorts1" /></div>
         </figure>
       </article>
     </section>
@@ -47,37 +47,37 @@
       <article>
         <h3>Accessories</h3>
         <figure>
-          <div><img src="../assets/img/cap (1).png" alt="accesories1" /></div>
+          <div><img src="../assets/img/Accessories/cap (1).png" alt="accesories1" /></div>
           <div>
-            <img src="../assets/img/sunglasses.png" alt="accesories2" />
+            <img src="../assets/img/Accessories/sunglasses.png" alt="accesories2" />
           </div>
-          <div><img src="../assets/img/sunhat(1).png" alt="accesories3" /></div>
+          <div><img src="../assets/img/Accessories/sunhat(1).png" alt="accesories3" /></div>
         </figure>
       </article>
       <article>
         <h3>Top</h3>
         <figure>
-          <div><img src="../assets/img/sleeves.png" alt="top1" /></div>
-          <div><img src="../assets/img/shirt (1).png" alt="top2" /></div>
+          <div><img src="../assets/img/Top/sleeves.png" alt="top1" /></div>
+          <div><img src="../assets/img/Top/shirt (1).png" alt="top2" /></div>
           <div>
-            <img src="../assets/img/shirt (2).png" alt="top3t" />
+            <img src="../assets/img/Top/shirt (2).png" alt="top3t" />
           </div>
         </figure>
       </article>
       <article>
         <h3>Bottoms</h3>
         <figure>
-          <div><img src="../assets/img/pants (3).png" alt="bottoms1" /></div>
-          <div><img src="../assets/img/loose-pants.png" alt="bottoms2" /></div>
-          <div><img src="../assets/img/pants (4).png" alt="bottoms3" /></div>
+          <div><img src="../assets/img/Bottoms/pants (1).png" alt="bottoms1" /></div>
+          <div><img src="../assets/img/Bottoms/trousers.png" alt="bottoms2" /></div>
+          <div><img src="../assets/img/Bottoms/loose-pants.png" alt="bottoms3" /></div>
         </figure>
       </article>
       <article>
         <h3>Shoes</h3>
         <figure>
-          <div><img src="../assets/img/sneakers.png" alt="shoes1" /></div>
-          <div><img src="../assets/img/sneakers (1).png" alt="shoes2" /></div>
-          <div><img src="../assets/img/sneakers (2).png" alt="shoes3" /></div>
+          <div><img src="../assets/img/shoes/sneakers.png" alt="shoes1" /></div>
+          <div><img src="../assets/img/shoes/sneakers (1).png" alt="shoes2" /></div>
+          <div><img src="../assets/img/shoes/sneakers (2).png" alt="shoes3" /></div>
         </figure>
       </article>
     </section>
@@ -90,37 +90,37 @@
       <article>
         <h3>Accessories</h3>
         <figure>
-          <div><img src="../assets/img/cap.png" alt="accesories1" /></div>
+          <div><img src="../assets/img/Accessories/cap.png" alt="accesories1" /></div>
           <div>
-            <img src="../assets/img/sunglasses.png" alt="accesories2" />
+            <img src="../assets/img/Accessories/sunglasses.png" alt="accesories2" />
           </div>
-          <div><img src="../assets/img/bucket.png" alt="accesories3" /></div>
+          <div><img src="../assets/img/Accessories/bucket.png" alt="accesories3" /></div>
         </figure>
       </article>
       <article>
         <h3>Top</h3>
         <figure>
-          <div><img src="../assets/img/coat (3).png" alt="top1" /></div>
-          <div><img src="../assets/img/cardigan.png" alt="top2" /></div>
+          <div><img src="../assets/img/Top/coat (3).png" alt="top1" /></div>
+          <div><img src="../assets/img/Top/cardigan.png" alt="top2" /></div>
           <div>
-            <img src="../assets/img/cardigan (1).png" alt="top3t" />
+            <img src="../assets/img/Top/cardigan (1).png" alt="top3t" />
           </div>
         </figure>
       </article>
       <article>
         <h3>Bottoms</h3>
         <figure>
-          <div><img src="../assets/img/pants (4).png" alt="bottoms3" /></div>
-          <div><img src="../assets/img/pants (3).png" alt="bottoms1" /></div>
-          <div><img src="../assets/img/loose-pants.png" alt="bottoms2" /></div>
+          <div><img src="../assets/img/Bottoms/pants (3).png" alt="bottoms3" /></div>
+          <div><img src="../assets/img/Bottoms/jeans.png" alt="bottoms1" /></div>
+          <div><img src="../assets/img/Bottoms/loose-pants.png" alt="bottoms2" /></div>
         </figure>
       </article>
       <article>
         <h3>Shoes</h3>
         <figure>
-          <div><img src="../assets/img/sneakers.png" alt="shoes1" /></div>
-          <div><img src="../assets/img/sneakers (1).png" alt="shoes2" /></div>
-          <div><img src="../assets/img/sneakers (2).png" alt="shoes3" /></div>
+          <div><img src="../assets/img/shoes/sneakers.png" alt="shoes1" /></div>
+          <div><img src="../assets/img/shoes/sneakers (1).png" alt="shoes2" /></div>
+          <div><img src="../assets/img/shoes/sneakers (2).png" alt="shoes3" /></div>
         </figure>
       </article>
     </section>
@@ -133,37 +133,37 @@
       <article>
         <h3>Accessories</h3>
         <figure>
-          <div><img src="../assets/img/beanie.png" alt="accesories1" /></div>
+          <div><img src="../assets/img/Accessories/beanie.png" alt="accesories1" /></div>
           <div>
-            <img src="../assets/img/scarf.png" alt="accesories2" />
+            <img src="../assets/img/Accessories/scarf.png" alt="accesories2" />
           </div>
-          <div><img src="../assets/img/socks.png" alt="accesories3" /></div>
+          <div><img src="../assets/img/Accessories/socks.png" alt="accesories3" /></div>
         </figure>
       </article>
       <article>
         <h3>Top</h3>
         <figure>
-          <div><img src="../assets/img/sweater.png" alt="top1" /></div>
-          <div><img src="../assets/img/sweater(1).png" alt="top2" /></div>
+          <div><img src="../assets/img/Top/sweater.png" alt="top1" /></div>
+          <div><img src="../assets/img/Top/sweater(1).png" alt="top2" /></div>
           <div>
-            <img src="../assets/img/sweater(2).png" alt="top3t" />
+            <img src="../assets/img/Top/sweater(2).png" alt="top3t" />
           </div>
         </figure>
       </article>
       <article>
         <h3>Bottoms</h3>
         <figure>
-          <div><img src="../assets/img/pants (3).png" alt="bottoms1" /></div>
-          <div><img src="../assets/img/loose-pants.png" alt="bottoms2" /></div>
-          <div><img src="../assets/img/pants (4).png" alt="bottoms3" /></div>
+          <div><img src="../assets/img/Bottoms/trousers.png" alt="bottoms1" /></div>
+          <div><img src="../assets/img/Bottoms/pants (3).png" alt="bottoms2" /></div>
+          <div><img src="../assets/img/Bottoms/pants (4).png" alt="bottoms3" /></div>
         </figure>
       </article>
       <article>
         <h3>Shoes</h3>
         <figure>
-          <div><img src="../assets/img/boots.png" alt="shoes1" /></div>
-          <div><img src="../assets/img/boots(1).png" alt="shoes2" /></div>
-          <div><img src="../assets/img/boots(2).png" alt="shoes3" /></div>
+          <div><img src="../assets/img/shoes/sneakers (2).png" alt="shoes1" /></div>
+          <div><img src="../assets/img/shoes/boots(1).png" alt="shoes2" /></div>
+          <div><img src="../assets/img/shoes/sneakers (1).png" alt="shoes3" /></div>
         </figure>
       </article>
     </section>
@@ -173,38 +173,38 @@
         <h3>Accessories</h3>
         <figure>
           <div>
-            <img src="../assets/img/winter-hat.png" alt="accesories1" />
+            <img src="../assets/img/Accessories/winter-hat.png" alt="accesories1" />
           </div>
           <div>
-            <img src="../assets/img/glove.png" alt="accesories2" />
+            <img src="../assets/img/Accessories/glove.png" alt="accesories2" />
           </div>
-          <div><img src="../assets/img/earmuffs.png" alt="accesories3" /></div>
+          <div><img src="../assets/img/Accessories/earmuffs.png" alt="accesories3" /></div>
         </figure>
       </article>
       <article>
         <h3>Top</h3>
         <figure>
-          <div><img src="../assets/img/coat.png" alt="top1" /></div>
-          <div><img src="../assets/img/coat (1).png" alt="top2" /></div>
+          <div><img src="../assets/img/Top/coat.png" alt="top1" /></div>
+          <div><img src="../assets/img/Top/coat (1).png" alt="top2" /></div>
           <div>
-            <img src="../assets/img/coat (2).png" alt="top3t" />
+            <img src="../assets/img/Top/coat (2).png" alt="top3t" />
           </div>
         </figure>
       </article>
       <article>
         <h3>Bottoms</h3>
         <figure>
-          <div><img src="../assets/img/winter.png" alt="bottoms1" /></div>
-          <div><img src="../assets/img/winter(1).png" alt="bottoms2" /></div>
-          <div><img src="../assets/img/winter(2).png" alt="bottoms3" /></div>
+          <div><img src="../assets/img/Bottoms/winter.png" alt="bottoms1" /></div>
+          <div><img src="../assets/img/Bottoms/winter(1).png" alt="bottoms2" /></div>
+          <div><img src="../assets/img/Bottoms/winter(2).png" alt="bottoms3" /></div>
         </figure>
       </article>
       <article>
         <h3>Shoes</h3>
         <figure>
-          <div><img src="../assets/img/boots.png" alt="shoes1" /></div>
-          <div><img src="../assets/img/boots(1).png" alt="shoes2" /></div>
-          <div><img src="../assets/img/boots(2).png" alt="shoes3" /></div>
+          <div><img src="../assets/img/shoes/boots.png" alt="shoes1" /></div>
+          <div><img src="../assets/img/shoes/boots(1).png" alt="shoes2" /></div>
+          <div><img src="../assets/img/shoes/boots(2).png" alt="shoes3" /></div>
         </figure>
       </article>
     </section>
